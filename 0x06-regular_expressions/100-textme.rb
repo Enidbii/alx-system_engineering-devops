5839e9bbf849c49238b8caaf9a01d9c06ed9f298
EnidBii <chebsenid@gmail.com>
2023-05-06 06:29:39 -0700

    textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-Fmt = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+Fmt = ARGV[0].scan(/from:(.\w+)| to:(.\w+)| flags:([0-9:-]+)/)
 message_list = [Fmt[0].compact, Fmt[1].compact, Fmt[2].compact] 
 
 puts message_list.join(,)
