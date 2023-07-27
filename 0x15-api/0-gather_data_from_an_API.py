57176f8fda589fb39590598c5b59a3a099eaf016
Enidbii <chebsenid@gmail.com>
2023-07-27 13:47:19 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,13 +7,22 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos",
-                         params={"userId": sys.argv[1]).json()
+def get_user_list():
+    emp_id = int(sys.argv[1])
+    url_1 = 'https://jsonplaceholder.typicode.com/users/%s' % emp_id
+    url_2 = '%s/todos' % url_1
+    list_todos = requests.get(url_2).json()
+    usr = requests.get(url_1).json()
+    complete_list = []
+    for todos in list_todos:
+        if todos.get('completed') is True:
+            complete_list.append(todos.get('title'))
 
-    com = [td.get("title") for td in todos if td.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-          user.get("name"), len(com), len(todos)))
-    [print("\t {}".format(comp)) for comp in com]
+    print("Employee {} is donemwith tasks({}/{}): "
+          .format(usr.get('name'), len(complet_list), len(list_todos)))
+    for todos in complete_list:
+        print("\t {}".format(todos))
+
+
+if __name__ == '__main__':
+    list_todos = get_user_list()
